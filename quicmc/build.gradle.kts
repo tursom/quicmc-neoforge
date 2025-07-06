f8a76f60365756999f1b31e92e4243a24fda91e8
tursom <tursom@foxmail.com>
2025-07-06 21:49:18 +0800

    清理 copy 来的 netty 类

--- quicmc/build.gradle.kts
+++ quicmc/build.gradle.kts
@@ -1,3 +1,28 @@
+val libraries by configurations.getting
+
+plugins {
+  id("com.gradleup.shadow") version "8.3.8"
+}
+
 dependencies {
   implementation(project(":netmix"))
+
+  val nettyQuicVersion = "0.0.72.Final"
+  libraries("io.netty.incubator:netty-incubator-codec-classes-quic:$nettyQuicVersion")
 }
+
+tasks.build {
+  dependsOn(tasks.shadowJar)
+}
+
+tasks.shadowJar {
+  dependencies {
+    exclude(dependency("^(?!io.netty.incubator).*:.*:.*"))
+  }
+
+  //finalizedBy("reobfShadowJar")
+}
+
+//reobf {
+//  create("shadowJar")
+//}
